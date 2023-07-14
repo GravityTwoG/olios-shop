a57305f627a2292e4ad141acd6c4bd463945ecbf
GravityTwoG <crytekov@gmail.com>
2023-07-14 19:02:41 +0700

    feat(backend): basket

--- backend/src/baskets/baskets.module.ts
+++ backend/src/baskets/baskets.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 
 import { BasketsController } from './baskets.controller';
 import { BasketsService } from './baskets.service';
+import { CartMapper } from './cart.mapper';
 
 @Module({
-  imports: [],
-  providers: [BasketsService],
+  imports: [ConfigModule],
+  providers: [BasketsService, CartMapper],
   controllers: [BasketsController],
   exports: [BasketsService],
 })
