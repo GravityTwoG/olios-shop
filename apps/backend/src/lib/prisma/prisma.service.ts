e8c86e17d88882846b4f141073af99150f762b83
GravityTwoG <crytekov@gmail.com>
2024-07-10 22:54:58 +0700

    feat: log prisma generated queries

--- apps/backend/src/lib/prisma/prisma.service.ts
+++ apps/backend/src/lib/prisma/prisma.service.ts
@@ -1,11 +1,44 @@
-import { Injectable, OnModuleInit } from '@nestjs/common';
-import { PrismaClient } from '@prisma/client';
+import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
+import { Prisma, PrismaClient } from '@prisma/client';
+
 import { createSearchQuery } from 'src/common/prisma/createSearchQuery';
 
+const clientOptions = {
+  log: [
+    {
+      emit: 'event',
+      level: 'query',
+    },
+    {
+      emit: 'stdout',
+      level: 'error',
+    },
+    {
+      emit: 'stdout',
+      level: 'info',
+    },
+    {
+      emit: 'stdout',
+      level: 'warn',
+    },
+  ],
+} satisfies Prisma.PrismaClientOptions;
+
 @Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit {
+export class PrismaService
+  extends PrismaClient<typeof clientOptions>
+  implements OnModuleInit
+{
+  private logger = new Logger(PrismaService.name);
+
   constructor() {
-    super({ log: ['query', 'error', 'info', 'warn'] });
+    super(clientOptions);
+
+    this.$on('query', (e) => {
+      this.logger.log('Query: ' + e.query);
+      this.logger.log('Params: ' + e.params);
+      this.logger.log('Duration: ' + e.duration + 'ms');
+    });
   }
 
   async onModuleInit() {
